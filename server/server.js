3b0cac77966300b5e3ecc611e5f70bd94a3cb0bb
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-09-11 22:23:52 +0300

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        //sending an object that contains an array adds flexibility for later edits
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
     console.log('started on port 3000')
 });
