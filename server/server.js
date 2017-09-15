cd20906c3dcc5710da2d2e9b00bddc59fbee52af
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-09-15 12:23:14 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash')
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -70,6 +71,35 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     });
      
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    //using lodash pick to filter out only the fields we want to update and prevent errors
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+
+    //check completed propery and update completedAt by adding a timestamp if it is done
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {;
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    //similar to the mongo native update arguments but done in mongoose
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
