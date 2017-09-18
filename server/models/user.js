7cda31b0af4036f0931e49aa428c961e55179777
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-09-18 17:36:54 +0300

    add GET /users/me and auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -42,7 +42,7 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email'])
 };
 
-//adding instance methods
+//adding instance methods = methods
 UserSchema.methods.generateAuthToken = function () {//using old syntax so we can bind .this keyword
 //arrow functions do not bind .this keyword
     var user = this;
@@ -54,6 +54,24 @@ UserSchema.methods.generateAuthToken = function () {//using old syntax so we can
 
     return user.save().then(() => {
         return token;
+    });
+};
+
+//model method = statics
+UserSchema.statics.findByToken = function (token) {
+    var User = this; //entire model is binded with .this
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    }catch(e){
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
