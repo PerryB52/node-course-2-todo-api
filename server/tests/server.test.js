055e5b84750517b0ac5d8ec6d6ccb16c0511ca8b
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-09-23 13:06:26 +0300

    added test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -297,11 +297,30 @@ describe('POST /todos', () => {
                         return done(err);
                     }
 
-                User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens.length).toBe(0);
-                    done();
-                }).catch((e) => done(e));
-            });
+                    User.findById(users[1]._id).then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    }).catch((e) => done(e));
+                });
+        });
+    });
+
+    describe('DELETE /users/me/token', () => {
+        it('should remove AUTH token on logout', (done) => {
+            request(app)
+                .delete('/users/me/token')
+                .set('x-auth', users[0].tokens[0].token)
+                .expect(200)
+                .end((err, res) => {
+                    if(err){
+                        return done(err)
+                    }
+
+                    User.findById(users[0]._id).then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    }).catch((e) => done(e));
+                });
         });
     });
 });
